d1f8c57ae986e3f462882a7fcc800e6966967093
zongm <zongmingyi1999@163.com>
2023-05-24 23:37:34 +0800

    注册时的异常

--- flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/Exception/InsertException.java
+++ flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/Exception/InsertException.java
@@ -1,23 +1,11 @@
 package cn.wolfcode.Exception;
 
+import cn.wolfcode.common.web.CodeMsg;
+
 public class InsertException extends BaseException{
-    public InsertException() {
-        super();
-    }
 
-    public InsertException(String message) {
-        super(message);
-    }
 
-    public InsertException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InsertException(Throwable cause) {
-        super(cause);
-    }
-
-    protected InsertException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public InsertException(CodeMsg codeMsg) {
+        super(codeMsg);
     }
 }
--- flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/Exception/InsertFailException.java
+++ flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/Exception/InsertFailException.java
@@ -1,23 +1,10 @@
 package cn.wolfcode.Exception;
 
+import cn.wolfcode.common.web.CodeMsg;
+
 public class InsertFailException extends BaseException{
-    public InsertFailException() {
-        super();
-    }
 
-    public InsertFailException(String message) {
-        super(message);
-    }
-
-    public InsertFailException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InsertFailException(Throwable cause) {
-        super(cause);
-    }
-
-    protected InsertFailException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public InsertFailException(CodeMsg codeMsg) {
+        super(codeMsg);
     }
 }
