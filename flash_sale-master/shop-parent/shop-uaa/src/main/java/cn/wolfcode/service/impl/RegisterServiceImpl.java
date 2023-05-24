39a5bbac152343edc0ad4a766531014b83f2de31
zongm <zongmingyi1999@163.com>
2023-05-24 23:38:33 +0800

    注册功能的业务层的实现类

--- flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/service/impl/RegisterServiceImpl.java
+++ flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/service/impl/RegisterServiceImpl.java
@@ -2,23 +2,48 @@ package cn.wolfcode.service.impl;
 
 import cn.wolfcode.Exception.InsertException;
 import cn.wolfcode.Exception.InsertFailException;
+import cn.wolfcode.Exception.PhoneIsNullException;
 import cn.wolfcode.domain.Register;
 import cn.wolfcode.mapper.RegisterMapper;
 import cn.wolfcode.service.IRegisterService;
+import cn.wolfcode.util.MD5Util;
+import cn.wolfcode.web.msg.RegisterCodeMsg;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
+
+@Service
 public class RegisterServiceImpl implements IRegisterService {
 
     private RegisterMapper registerMapper;
     @Override
     public void register(Register register) {
+        //判断手机号是否为空
+        if(null == register.getPhone()){
+            throw new PhoneIsNullException(RegisterCodeMsg.PHONE_IS_NULL);
+        }
         //查询是否该手机号已被注册
         Register result = registerMapper.searchByPhone(register.getPhone());
         if(result != null){
-            throw new InsertException("该手机号已被注册");
+            throw new InsertException(RegisterCodeMsg.DUPLICATE_REGISTER);
         }
+        //加密密码
+        //生成盐值
+        String randomUUID = UUID.randomUUID().toString().toUpperCase();
+        String MD5Password = MD5Util.encode(register.getPassword(), randomUUID);
+        register.setPassword(MD5Password);
+        register.setSalt(randomUUID);
+        //将剩余的字段补充
+        register.setCreateUser(String.valueOf(register.getPhone()));
+        register.setCreateTime(new Date());
+        register.setModifyUser(String.valueOf(register.getPhone()));
+        register.setModifyTime(new Date());
+
         int rows = registerMapper.register(register);
         if(rows != 1){
-            throw new InsertFailException("出现未知异常，注册失败");
+            throw new InsertFailException(RegisterCodeMsg.REGISTER_FAIL);
         }
     }
 }
