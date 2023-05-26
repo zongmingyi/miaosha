03f387425ea9f672446bb8546f0670befe3d6df8
zongm <zongmingyi1999@163.com>
2023-05-26 22:35:13 +0800

    base实体字段

--- flash_sale-master/shop-parent/shop-common/src/main/java/cn/wolfcode/common/BaseEntity/BaseField.java
+++ flash_sale-master/shop-parent/shop-common/src/main/java/cn/wolfcode/common/BaseEntity/BaseField.java
@@ -1,4 +1,4 @@
-package cn.wolfcode.commonField;
+package cn.wolfcode.common.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -6,7 +6,7 @@ import lombok.Setter;
 import java.util.Date;
 
 @Getter@Setter
-public class CommonField {
+public class BaseField {
     private String createUser;//创建者
     private Date createTime;//创建时间
     private String modifyUser;//修改者
