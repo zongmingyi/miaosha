7266ce212d93ec5baac65417a25df217641c9206
zongm <zongmingyi1999@163.com>
2023-05-26 22:37:07 +0800

    注册实体

--- flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/domain/Register.java
+++ flash_sale-master/shop-parent/shop-uaa/src/main/java/cn/wolfcode/domain/Register.java
@@ -1,11 +1,11 @@
 package cn.wolfcode.domain;
 
-import cn.wolfcode.commonField.CommonField;
+import cn.wolfcode.common.BaseEntity.BaseField;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter@Setter
-public class Register extends CommonField {
+public class Register extends BaseField {
     private Long phone;//用户注册电话
     private String password;//密码
     private String salt;//盐值
